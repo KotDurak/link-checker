66825bb83104536e38f8a71c42afaef7f78c8305
KotDurak <DarkProkuror@mail.ru>
2021-01-28 22:22:03 +0400

    first

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('title', 'Link checker')
+
+@section('sidebar')
+    @parent
+@endsection
+
+@section('content')
+    <div class="row">
+        <p>Link checker main page</p>
+    </div>
+@endsection
